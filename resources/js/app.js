96a87993c77640429fb2e61e0f8c1b1bf2594d62
danielmestre98 <dmestreloureiro@gmail.com>
2024-03-12 20:33:12 +0000

    Configuração inicial do vue, inertia e quasar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
-import './bootstrap';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { Quasar } from "quasar";
+import quasarLang from "quasar/lang/pt-BR";
+import quasarIconSet from "quasar/icon-set/svg-fontawesome-v6";
+
+// Import icon libraries
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(Quasar, {
+                plugins: {},
+                lang: quasarLang,
+                iconSet: quasarIconSet,
+            })
+            .mount(el);
+    },
+});
